22a7730dbb399163e23523e18af515d9221691e0
Jhurtadom76 <jhurtadoma@unal.edu.co>
2026-06-09 14:31:38 -0500

    Lab02Try2

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -1,4 +1,19 @@
+"""
+Escriba el codigo que ejecute la accion solicitada en cada pregunta. Los
+datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y 
+`tbl2.tsv`. En este laboratorio solo puede utilizar las funciones y 
+librerias de pandas para resolver las preguntas.
+"""
 import pandas as pd
+
 def pregunta_01():
-    df = pd.read_csv("tbl0.tsv", sep="\t")
+    df = pd.read_csv("files/input/tbl0.tsv", sep="\t")
+
     return df.shape[0]
+    """
+    ¿Cuál es la cantidad de filas en la tabla `tbl0.tsv`?
+
+    Rta/
+    40
+
+    """
--- homework/pregunta_13.py
+++ homework/pregunta_13.py
@@ -1,7 +1,28 @@
+"""
+Escriba el codigo que ejecute la accion solicitada en cada pregunta. Los
+datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y 
+`tbl2.tsv`. En este laboratorio solo puede utilizar las funciones y 
+librerias de pandas para resolver las preguntas.
+"""
 import pandas as pd
-def pregunta_13():
 
-    t0 = pd.read_csv("tbl0.tsv", sep="\t")
-    t2 = pd.read_csv("tbl2.tsv", sep="\t")
-    m = pd.merge(t0, t2, on="c0")
-    return m.groupby("c1")["c5b"].sum()
+def pregunta_13():  
+    df_1 = pd.read_csv("files/input/tbl0.tsv", sep="\t")
+    df_2 = pd.read_csv("files/input/tbl2.tsv", sep="\t")
+
+    merg_df = pd.merge(df_1, df_2, on="c0")
+
+    return merg_df.groupby("c1")["c5b"].sum()
+    """
+    Si la columna `c0` es la clave en los archivos `tbl0.tsv` y `tbl2.tsv`,
+    compute la suma de `tbl2.c5b` por cada valor en `tbl0.c1`.
+
+    Rta/
+    c1
+    A    146
+    B    134
+    C     81
+    D    112
+    E    275
+    Name: c5b, dtype: int64
+    """
